433ded708fc9a0daadc99148162ee536d94b742b
MaelleMrt <64894417+MaelleMrt@users.noreply.github.com>
2020-06-15 15:14:43 +0200

    Add files via upload

---
+++ Portage-Java-v0/src/princetonPlainsboro/TentativeConnexion.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package princetonPlainsboro;
+
+/**
+ *
+ * @author Maelle
+ */
+public class TentativeConnexion {
+
+    private Hospital hospital;
+
+    public TentativeConnexion(Hospital hospital) {
+        this.hospital = hospital;
+    }
+
+    public boolean connexionValide(String identifiant, String mdp, String statut) {
+        if (statut.equals("Medecin")) {
+            for (Medecin m : this.hospital.getListMedecin()) {
+                if (m.getIdentifiant().equals(identifiant) && m.getMdp().equals(mdp)) {
+                    return true;
+                }
+            }
+        }
+        if (statut.equals("Secretaire Medicale")) {
+            for (SecretaireMedicale sm : this.hospital.getListSM()) {
+                if (sm.getIdentifiant().equals(identifiant) && sm.getMdp().toCharArray().equals(mdp)) {
+                    return true;
+                }
+            }
+        }
+        if (statut.equals("Secretaire Administrative")) {
+            if (this.hospital.getSA().getIdentifiant().equals(identifiant) && this.hospital.getSA().getMdp().toCharArray().equals(mdp)) {
+                return true;
+            }
+        } 
+        return false;
+    }
+}
