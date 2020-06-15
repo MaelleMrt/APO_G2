e2d1483e5e339c7105e0e1f05ebcc07fabe86296
MaelleMrt <64894417+MaelleMrt@users.noreply.github.com>
2020-06-15 15:01:03 +0200

    Add files via upload

--- Portage-Java-v0/src/Interfaces/Connexion.java
+++ Portage-Java-v0/src/Interfaces/Connexion.java
@@ -5,17 +5,24 @@
  */
 package Interfaces;
 
+import princetonPlainsboro.Hospital;
+import princetonPlainsboro.LectureXMLHop;
+import princetonPlainsboro.TentativeConnexion;
+
 /**
  *
  * @author Maelle
  */
 public class Connexion extends javax.swing.JFrame {
 
+    private Hospital hospital;
+
     /**
      * Creates new form Connexion
      */
-    public Connexion() {
+    public Connexion(Hospital hospital) {
         initComponents();
+        this.hospital = hospital;
     }
 
     /**
@@ -37,6 +44,9 @@ public class Connexion extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -51,9 +61,26 @@ public class Connexion extends javax.swing.JFrame {
 
         jPasswordField1.setText("jPasswordField1");
 
-        jToggleButton1.setText("?");
+        jToggleButton1.setText("✓");
+        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton1ActionPerformed(evt);
+            }
+        });
 
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/logo2.png"))); // NOI18N
+        jLabel6.setFont(new java.awt.Font("Bell MT", 0, 13)); // NOI18N
+        jLabel6.setIcon(new javax.swing.ImageIcon("/Users/Maelle/Desktop/projet APO/Portage-Java-v0/src/images/logo2.png")); // NOI18N
+
+        jComboBox1.setFont(new java.awt.Font("Bell MT", 0, 13)); // NOI18N
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Medecin", "Secretaire Administrative", "Secretaire Medicale", " " }));
+
+        jLabel7.setFont(new java.awt.Font("Bell MT", 0, 13)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(234, 24, 24));
+        jLabel7.setText("Champs incomplets");
+
+        jLabel8.setFont(new java.awt.Font("Bell MT", 0, 13)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(233, 31, 31));
+        jLabel8.setText("Mot de passe ou identifiant incorrect");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -62,16 +89,16 @@ public class Connexion extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(158, 158, 158)
-                        .addComponent(jLabel5))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(32, 32, 32)
                         .addComponent(jLabel1))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(424, 424, 424)
+                        .addGap(158, 158, 158)
+                        .addComponent(jLabel5)
+                        .addGap(266, 266, 266)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jPasswordField1, javax.swing.GroupLayout.DEFAULT_SIZE, 186, Short.MAX_VALUE)
-                            .addComponent(jTextField1))))
+                            .addComponent(jTextField1)
+                            .addComponent(jComboBox1, 0, 0, Short.MAX_VALUE))))
                 .addGap(48, 48, 48)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING)
@@ -82,12 +109,21 @@ public class Connexion extends javax.swing.JFrame {
                         .addContainerGap())))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jToggleButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(219, 219, 219))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jToggleButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(206, 206, 206))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel7)
+                        .addGap(66, 66, 66))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel8)
+                        .addGap(14, 14, 14))))
             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createSequentialGroup()
                     .addContainerGap()
-                    .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, 751, Short.MAX_VALUE)))
+                    .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 378, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(373, Short.MAX_VALUE)))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -102,24 +138,30 @@ public class Connexion extends javax.swing.JFrame {
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jLabel5)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
+                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jLabel3)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(28, 28, 28)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jToggleButton1)
-                        .addContainerGap(141, Short.MAX_VALUE))))
+                    .addComponent(jLabel3)
+                    .addComponent(jToggleButton1))
+                .addGap(30, 30, 30)
+                .addComponent(jLabel7)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel8)
+                .addContainerGap(42, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel1Layout.createSequentialGroup()
-                    .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, 312, Short.MAX_VALUE)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                    .addContainerGap(48, Short.MAX_VALUE)
+                    .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 264, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addContainerGap()))
         );
+
+        jLabel7.setVisible(false);
+        jLabel8.setVisible(false);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -134,6 +176,54 @@ public class Connexion extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
+        if (jToggleButton1.isSelected()) {
+            TentativeConnexion connexionV = new TentativeConnexion(hospital);
+            if (jTextField1.getText().isEmpty() | jPasswordField1.getPassword().toString().isEmpty()) {
+                jLabel7.setVisible(true);
+                jToggleButton1.setSelected(false);
+                jLabel8.setVisible(false);
+            }
+            else if (connexionV.connexionValide(jTextField1.getText(), String.valueOf(jPasswordField1.getPassword()), jComboBox1.getSelectedItem().toString())) {
+                //si on a les bons identifiants on se connecte -> disparaitre les messages d'erreurs
+                //vider les JtextFields
+                jLabel7.setVisible(false);
+                jLabel8.setVisible(false);
+                jTextField1.setText("Nom Utilisateur");
+                jPasswordField1.setText("");
+                if (jComboBox1.getSelectedItem().toString() == "Medecin") {
+                    System.out.println("connexion medecin ok ");
+                    //ouerture de l'interface medecin
+                    ///à faire !!!
+                    //new InterfaceMedecin(hospital.rechercherMedecin(jTextField1.getText()));
+
+                    //on ferme la fenetre de connexion
+                    this.setVisible(false);
+
+                }
+                if (jComboBox1.getSelectedItem().toString() == "Secretaire Administrative") {
+
+                    //new Secretaire_Administrative(hospital.getSA());
+                    //on ferme la fenetre de connexion
+                    this.setVisible(false);
+
+                }
+                if (jComboBox1.getSelectedItem().toString() == "Secretaire Medicale") {
+
+                    //new SecretaireMedicale(hospital.rechercherSM(jTextField1.getText()));
+                    //on ferme la fenetre de connexion
+                    this.setVisible(false);
+                }
+            } else {
+                System.out.println("identifiant: "+jTextField1.getText());
+                System.out.println("mdp: "+jPasswordField1.getPassword().toString().toCharArray());
+                jLabel8.setVisible(true);
+                jLabel7.setVisible(false);
+                jToggleButton1.setSelected(false);
+            }
+        }
+    }//GEN-LAST:event_jToggleButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -165,18 +255,24 @@ public class Connexion extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Connexion().setVisible(true);
+                
+                LectureXMLHop test = new LectureXMLHop("hopital.xml");
+                Hospital hop = test.getHospital();
+                new Connexion(hop).setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPasswordField jPasswordField1;
     private javax.swing.JTextField jTextField1;
