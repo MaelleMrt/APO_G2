a62668984fe39b935cf2de22243a4f2802f1b9c2
MaelleMrt <64894417+MaelleMrt@users.noreply.github.com>
2020-06-18 19:43:48 +0200

    Add files via upload

---
+++ Portage-Java-v0/src/princetonPlainsboro/StringChecker.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package princetonPlainsboro;
+
+/**
+ *
+ * @author Maelle
+ */
+public class StringChecker {
+        public static boolean chekerString(String numero) {
+        for (int i = 0; i < numero.length(); i++) {
+            if (Character.toUpperCase(numero.charAt(i)) >= ':' && Character.toUpperCase(numero.charAt(i)) <= '@') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
